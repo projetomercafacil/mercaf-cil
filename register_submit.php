ea5b09e28b2fe34045f38e7922268db2dc1b7211
projetomercafacil <projetomercafacil@gmail.com>
2025-09-01 19:51:33 -0300

    Delete register_submit.php

--- register_submit.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../app/Controllers/AuthController.php';
-$auth = new AuthController();
-$auth->register(
-    $_POST['nome'] ?? '',
-    $_POST['email'] ?? '',
-    $_POST['senha'] ?? '',
-    $_POST['confirmar'] ?? ''
-);
