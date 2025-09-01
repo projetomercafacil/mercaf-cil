c84d01e8efd34999d9a884639617c975158c4af4
projetomercafacil <projetomercafacil@gmail.com>
2025-09-01 19:48:23 -0300

    Delete forgot.php

--- forgot.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../app/Controllers/AuthController.php';
-$auth = new AuthController();
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $auth->sendResetLink($_POST['email'] ?? '');
-    exit;
-}
-$auth->forgotPassword();
