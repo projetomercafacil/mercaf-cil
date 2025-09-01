4d23b344bebe21a1145c35fee3430ba4b35949e0
projetomercafacil <projetomercafacil@gmail.com>
2025-09-01 19:48:04 -0300

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// config/config.php
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'mercafacil');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-
-// URL base para montar links (ajuste se sua pasta não for /mercafacil)
-define('BASE_URL', '/mercafacil/public');
