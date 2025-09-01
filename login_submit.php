2eec1fbfe0f96f0719535b4d641fcc5db12974cb
projetomercafacil <projetomercafacil@gmail.com>
2025-09-01 19:48:54 -0300

    Delete login_submit.php

--- login_submit.php
+++
@@ -1,5 +0,0 @@
-<?php
-// separando POST do template
-require_once __DIR__ . '/../app/Controllers/AuthController.php';
-$auth = new AuthController();
-$auth->login($_POST['email'] ?? '', $_POST['senha'] ?? '');
